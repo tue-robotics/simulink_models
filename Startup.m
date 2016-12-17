e6caaa2cdf82e9c6ea6137de8fe25c02918e0e06
Max Baeten <m.j.j.baeten@gmail.com>
2016-12-17 17:57:15 +0100

    added sergio arm mdl

---
+++ Startup.m
@@ -0,0 +1,15 @@
+%% Startup file for setting up matlab workspace
+
+% Current path
+cd ..
+pathstring = pwd;
+cd simulink_models;
+
+% Adding simulink models folder
+addpath(genpath(strcat(pathstring,'\simulink_models')))
+
+% Adding tue simulink ethercaqt folder
+addpath(genpath(strcat(pathstring,'\tue_simulink_ethercat')))
+
+% Load Constants
+TUe.Reload()
